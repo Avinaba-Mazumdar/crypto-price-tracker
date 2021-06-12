a92a46abdf370b648e2ae0f391999a42d9486cec
Avinaba Mazumdar <avinaba2@gmail.com>
2021-06-12 16:57:44 +0530

    interchanged volume and market cap

--- src/Coin.js
+++ src/Coin.js
@@ -11,16 +11,16 @@ const Coin = ({image, name, symbol, price, volume, priceChange, marketcap}) => {
                               <p className='coin-symbol'>{symbol}</p>
                         </div>
                         <div className="coin-data">
-                              <p className="coin-price">{price}</p>
-                              <p className="coin-marketcap">{volume.toLocaleString()}</p>
+                              <p className="coin-price">${price}</p>
+                              <p className="coin-volume">${volume.toLocaleString()}</p>
                               {priceChange < 0 ? (
                                           <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
                                     ) : (
                                           <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
                                     )
                               }
-                              <p className="coin-volume">
-                                    Mkt Cap: ${marketcap.toLocaleString()}
+                              <p className="coin-marketcap">
+                                    ${marketcap.toLocaleString()}
                               </p>
                         </div>
                   </div>
