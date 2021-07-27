4ba68f5dc463a5c00c3801ea57331c26ed34e32b
Avinaba-Mazumdar <avinaba2@gmail.com>
2021-07-27 16:54:44 +0530

    introduced table format

--- src/Coin.js
+++ src/Coin.js
@@ -3,9 +3,26 @@ import './Coin.css'
 
 const Coin = ({image, name, symbol, price, volume, priceChange, marketcap}) => {
       return (
-            <div className='coin-container'>
-                  
-            </div>
+            <tr>
+                  <th className='coin-th'><img src={image} alt='crypto'/></th>
+                  <th className='coin-th'><h1>{name}</h1></th>
+                  <th className='coin-th'><p className='coin-symbol'>{symbol}</p></th>
+                  <th className='coin-th'><p>${price}</p></th>
+                  <th className='coin-th volume'><p>${volume.toLocaleString()}</p></th>
+                  <th className='coin-th'>
+                        {priceChange < 0 ? (
+                              <p className='red'>{priceChange.toFixed(2)}%</p>
+                                    ) : (
+                                          <p className='green'>{priceChange.toFixed(2)}%</p>
+                                    )
+                              }
+                  </th>
+                  <th className='coin-th mktcap'>
+                        <p className="coin-marketcap">
+                                    ${marketcap.toLocaleString()}
+                        </p>
+                  </th>
+            </tr>
       )
 }
 
